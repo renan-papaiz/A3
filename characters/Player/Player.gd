3320c1eb5cdf498728114464aa437e32099ee48c
renan-papaiz <renanrenanrspx@gmail.com>
2023-05-28 14:16:34 -0300

    Commit inicial

---
+++ characters/Player/Player.gd
@@ -0,0 +1,65 @@
+extends KinematicBody
+
+var hotkeys = {
+	KEY_1: 0,
+	KEY_2: 1,
+	KEY_3: 2,
+	KEY_4: 3
+}
+
+export var mouse_sens = 0.065
+
+onready var camera = $Camera
+onready var character_mover = $CharacterMover
+onready var health_manager = $HealthManager
+onready var weapon_manager = $Camera/WeaponManager
+
+var dead = false
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	character_mover.init(self)
+	health_manager.init()
+	health_manager.connect("dead", self, "kill")
+	
+func _process(delta):
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
+		
+	if dead:
+		return
+		
+	var move_vec = Vector3()
+	if Input.is_action_pressed("move_forward"):
+		move_vec += Vector3.FORWARD
+	if Input.is_action_pressed("move_backward"):
+		move_vec += Vector3.BACK
+	if Input.is_action_pressed("move_left"):
+		move_vec += Vector3.LEFT
+	if Input.is_action_pressed("move_right"):
+		move_vec += Vector3.RIGHT
+	character_mover.set_move_vec(move_vec)
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= mouse_sens * event.relative.x
+		camera.rotation_degrees.x -= mouse_sens * event.relative.y
+		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
+	if event is InputEventKey and event.pressed:
+		if event.scancode in hotkeys:
+			weapon_manager.switch_to_weapon_slot(hotkeys[event.scancode])
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == BUTTON_WHEEL_DOWN:
+			weapon_manager.switch_to_next_weapon()
+		if event.button_index == BUTTON_WHEEL_UP:
+			weapon_manager.switch_to_last_weapon()
+		
+func hurt(damage, dir):
+	health_manager.hurt(damage, dir)
+	
+func heal(amount):
+	health_manager.heal(amount)
+
+func kill():
+	dead = true
+	character_mover.freeze()
