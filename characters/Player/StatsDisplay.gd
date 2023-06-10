07194510a591a37ab1ffb28aec883072b06eb144
renan-papaiz <renanrenanrspx@gmail.com>
2023-06-10 14:39:05 -0300

    commit 12

---
+++ characters/Player/StatsDisplay.gd
@@ -0,0 +1,17 @@
+extends Label
+
+var ammo = 0
+var health = 0
+
+func update_ammo(amnt):
+	ammo = amnt
+	update_display()
+	
+func update_health(amnt):
+	health = amnt
+	update_display()
+	
+func update_display():
+	text = "Health: " + str(health)
+	var ammo_amnt = str(ammo)
+	text += "\nAmmo: " + ammo_amnt
